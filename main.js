d59610f94c04c4cf80073c451839c9a342698392
muhammed elrimi <mohamadrimi12345@gmail.com>
2024-03-23 03:45:39 +0300

    fısrt challenge

---
+++ main.js
@@ -0,0 +1,67 @@
+document.write("hello");
+// var user ="muhammed";
+// console.log(user);
+// console.log(hello);
+// let a="we love";
+// let b="javascript";
+// document.write(a+" "+b);
+// console.log(a , b);
+// let m ="ali";
+// let n ="muhammed";
+// let c="hani";
+// let d="mahir";
+// console.log(` ${m}      ${n}${c}${d}`);
+// let a ="tital";
+// let des="elzero web school";
+// let mup =`
+// <div class="parent">
+// <div class="child">
+// <h3>${a}</h3>
+// <p>${des}</p>
+// </div> </div>`;
+// console.log(mup);
+// document.write(mup);
+let a ="Elzero" 
+ var b ="Elzero Web School";
+const s ="25 /10";
+let ma = `
+
+<div >
+<h3>${a}</h3>
+<p>${b}</p>
+<span>${s}</span>
+</div>
+
+`;
+let mb = `
+
+<div >
+<h3>${a}</h3>
+<p>${b}</p>
+<span>${s}</span>
+</div>
+
+`;
+let mc = `
+
+<div >
+<h3>${a}</h3>
+<p>${b}</p>
+<span>${s}</span>
+</div>
+
+`;
+let md = `
+
+<div >
+<h3>${a}</h3>
+<p>${b}</p>
+<span>${s}</span>
+</div>
+
+`;
+
+document.write(ma);
+document.write(mb);
+document.write(mc);
+document.write(md);
