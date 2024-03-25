d85e17f8e6baffcb57fc2cb3f683023470903363
muhammed elrimi <mohamadrimi12345@gmail.com>
2024-03-25 06:24:33 +0300

    3.th challenge

--- main.js
+++ main.js
@@ -70,16 +70,22 @@ document.write("hello");
 // let c = 80;
 // console.log(++a + +b++  + +c++ - +a++);
 // console.log(++a + -b  + +c++ - -a++ + +a);
-let a = 1_00;
-let b = 2_00.5;
-let c = 1e2;
-let d =2.4;
-console.log(Math.trunc(Math.min(a , b , c, d)));
-document.write(c);
-console.log(a**Math.trunc(d));
-console.log(Math.floor(d));
-console.log(Math.ceil(d));
-console.log(Math.trunc(d));
-console.log(parseInt(d));
-console.log((Math.floor(b) / Math.ceil(d)).toFixed(2));
-console.log(Math.ceil(Math.floor(b) / Math.ceil(d)));
+// let a = 1_00;
+// let b = 2_00.5;
+// let c = 1e2;
+// let d =2.4;
+// console.log(Math.trunc(Math.min(a , b , c, d)));
+// document.write(c);
+// console.log(a**Math.trunc(d));
+// console.log(Math.floor(d));
+// console.log(Math.ceil(d));
+// console.log(Math.trunc(d));
+// console.log(parseInt(d));
+// console.log((Math.floor(b) / Math.ceil(d)).toFixed(2));
+// console.log(Math.ceil(Math.floor(b) / Math.ceil(d)));
+let a ="Elzero Web School";
+console.log(a.toUpperCase().charAt(2)+a.slice(3,6)); //Zero
+console.log(a.toUpperCase().substring(13 , 14).repeat(8));   //HHHHHHHH
+console.log(a.split(" ",1));
+console.log(a.substr(0 , 7) + a.substr(length - 7)); // Elzero School
+console.log(a.charAt(0).toLowerCase()+a.substring(1 , a.length - 1).toUpperCase()+a.charAt(a.length - 1).toLowerCase()); // eLZERO WBE SCHOOl
