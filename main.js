864b419508921cbe3632f4abb9a489d3cc516488
muhammed elrimi <mohamadrimi12345@gmail.com>
2024-03-25 09:17:43 +0300

    4.th challenge

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ document.write("hello");
 // </div> </div>`;
 // console.log(mup);
 // document.write(mup);
-// let a ="Elzero" 
+// let a ="Elzero"
 //  var b ="Elzero Web School";
 // const s ="25 /10";
 // let ma = `
@@ -83,9 +83,69 @@ document.write("hello");
 // console.log(parseInt(d));
 // console.log((Math.floor(b) / Math.ceil(d)).toFixed(2));
 // console.log(Math.ceil(Math.floor(b) / Math.ceil(d)));
-let a ="Elzero Web School";
-console.log(a.toUpperCase().charAt(2)+a.slice(3,6)); //Zero
-console.log(a.toUpperCase().substring(13 , 14).repeat(8));   //HHHHHHHH
-console.log(a.split(" ",1));
-console.log(a.substr(0 , 7) + a.substr(length - 7)); // Elzero School
-console.log(a.charAt(0).toLowerCase()+a.substring(1 , a.length - 1).toUpperCase()+a.charAt(a.length - 1).toLowerCase()); // eLZERO WBE SCHOOl
+let a = "Elzero Web School";
+console.log(a.toUpperCase().charAt(2) + a.slice(3, 6)); //Zero
+console.log(a.toUpperCase().substring(13, 14).repeat(8)); //HHHHHHHH
+console.log(a.split(" ", 1));
+console.log(a.substr(0, 7) + a.substr(length - 7)); // Elzero School
+console.log(
+  a.charAt(0).toLowerCase() +
+    a.substring(1, a.length - 1).toUpperCase() +
+    a.charAt(a.length - 1).toLowerCase()
+); // eLZERO WBE SCHOOl
+let price = 0;
+console.log(` the price is ${price || 200}  `);
+console.log(`the price is ${price ?? 200}`);
+let s = 10;
+s < 10
+  ? console.log(10)
+  : s >= 10 && s <= 40
+  ? console.log("10 to 40")
+  : s > 40
+  ? console.log(" > 40")
+  : console.log(unknown); //10 to 20
+let st = "Elzero Web School";
+if (st.length * 2 == 354) {
+  console.log("good");
+} else if (st.charAt(st.indexOf("W")).toLowerCase() === "m") {
+  console.log("gooooood");
+} else if (typeof st.length === "number1") {
+  console.log("wrong");
+} else if (st.substr(0, 6).repeat(2) === "ElzerElzero") {
+  console.log("amazing");
+} else if (typeof st.length !== "string") {
+  console.log("wowww");
+}
+let jop = "Support";
+let salary = 0;
+switch (jop) {
+  case "manager":
+    salary = 8000;
+    console.log(`the salary is ${salary}`);
+    break;
+  case "IT":
+  case "Support":
+    salary = 6000;
+    console.log(`the salary is ${salary}`);
+    break;
+  case "Developer ":
+    salary = 7000;
+    console.log(`the salary is ${salary}`);
+    break;
+  default:
+    console.log("no one");
+}
+let holoday = 2;
+let money = 0;
+if (holoday == 0) {
+  money = 5000;
+  console.log(`my money is${money}`);
+} else if (holoday == 1 ||  holoday == 2) {
+  money = 3000;
+  console.log(`my money is ${money}`);
+} else if (holoday == 3) {
+  money = 2000;
+  console.log(`my money is${money}`);
+} else {
+  console.log(`my money is finshed`);
+}
