731a07736090fcabdb8fda92ee7b3eeb8a48c03e
muhammed elrimi <mohamadrimi12345@gmail.com>
2024-04-01 23:52:53 +0300

    function

--- main.js
+++ main.js
@@ -247,7 +247,11 @@
 // showDetails("ali", 24, true);
 // // showDetails("muhammed\n", false, 45);
 // document.write(` Hello:${first}  your  age is ${second}  ${third}`);
-function calc( number1 ,number2){
-  return number1 + number2 ;
+// function calc( number1 ,number2){
+//   return number1 + number2 ;
+// }
+// console.log(calc(12 , 34 ));
+let calcoletr = function( number1, number2){
+return number1 + number2;
 }
-console.log(calc(12 , 34 ));
+console.log(calcoletr(12,55));
